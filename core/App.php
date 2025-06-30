09becd9f6fe7c503105f8cb1200287613cff3b05
t-ho <t.ho@tdev.app>
2025-06-30 11:27:29 -0700

    Add dependency injection container and app configuration

---
+++ core/App.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core;
+
+class App
+{
+    protected static $container = [];
+
+    public static function bind(string $key, mixed $value): void
+    {
+        static::$container[$key] = $value;
+    }
+
+    public static function get(string $key): mixed
+    {
+        if (!array_key_exists($key, static::$container)) {
+            throw new \Exception("Error: Key not found in container: {$key}");
+        }
+
+        return static::$container[$key];
+    }
+}
