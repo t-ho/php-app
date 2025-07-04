5176890e7919c215403fc9073c63fddced485f00
t-ho <t.ho@tdev.app>
2025-07-03 17:55:44 -0700

    Add .env.example file

--- config.php
+++ config.php
@@ -2,11 +2,16 @@
 
 return [
   'app' => [
-    'name' => 'My Blog',
-    'debug' => true,
+    'name' => $_ENV['APP_NAME'] ?? 'PHP App',
+    'debug' => $_ENV['APP_DEBUG'] ?? true,
+    'env' => $_ENV['APP_ENV'] ?? 'development',
   ],
   'database' => [
-    'driver' => 'sqlite',
-    'dbname' => __DIR__ . '/database/blog.sqlite',
+    'driver' => 'mysql',
+    'host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'dbname' => $_ENV['DB_NAME'] ?? 'testing_db',
+    'username' => $_ENV['DB_USER'] ?? '',
+    'password' => $_ENV['DB_PASS'] ?? '',
+    'port' => $_ENV['DB_PORT'] ?? 3306,
   ]
 ];
