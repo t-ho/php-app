a2405154366956f204f57aba75004af1d8ca1e32
t-ho <t.ho@tdev.app>
2025-07-07 15:52:00 -0700

    Add ViteHelper for smart asset management and cache busting

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Load helper functions before error handlers
+foreach (glob(__DIR__ . '/app/Helpers/*.php') as $file) {
+    require_once $file;
+}
+
 use App\Core\App;
 use App\Core\Database;
 use App\Core\ErrorHandler;
+use App\Helpers\ViteHelper;
 
 set_exception_handler([ErrorHandler::class, 'handleException']);
 set_error_handler([ErrorHandler::class, 'handleError']);
@@ -15,3 +21,4 @@ $config = require_once __DIR__ . '/config.php';
 
 App::bind('config', $config);
 App::bind('database', new Database($config['database']));
+App::bind('vite', new ViteHelper());
